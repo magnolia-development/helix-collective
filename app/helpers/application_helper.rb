c5a5bf03d572e548cd1f5b0b31673c6331dddb11
Malak Janus <992688+malakada@users.noreply.github.com>
2024-10-01 14:51:30 -0700

    Rudimentary main nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def main_nav_items
+    [
+      { nav_link: "/", nav_text: "Home" },
+      { nav_link: "/events", nav_text: "Events" },
+      { nav_link: "/vision", nav_text: "Vision" },
+      { nav_link: "/resources", nav_text: "Resources" },
+    ]
+  end
 end
 
