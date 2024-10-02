b227f89f66d7504f5e2a02c068cf70caeda96a2b
Malak Janus <992688+malakada@users.noreply.github.com>
2024-10-02 14:41:24 -0700

    Add info for subnav and start styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,63 @@ module ApplicationHelper
   def main_nav_items
     [
       { nav_link: "/", nav_text: "Home" },
-      { nav_link: "/events", nav_text: "Events" },
-      { nav_link: "/vision", nav_text: "Vision" },
-      { nav_link: "/resources", nav_text: "Resources" },
+      { nav_link: "/solve", nav_text: "Solve", sub_nav: {
+        style: "mega_columns",
+        subtitle: "We help you solve your biggest challenges.",
+        sub_description: "Our network of experts can help you with a wide range of problems. We have experience in a variety of industries and can provide solutions for your business.",
+        items: [
+          { group_name: "Industries", items: [
+            { nav_link: "/solve/healthcare", nav_text: "Healthcare" },
+            { nav_link: "/solve/ed-tech", nav_text: "Ed Tech" },
+            { nav_link: "/solve/nonprofits", nav_text: "Non-profits" },
+            { nav_link: "/solve/e-commerce", nav_text: "Ecommerce" },
+          ] },
+          { group_name: "Solutions", items: [
+            { nav_link: "/solve/marketing", nav_text: "Marketing" },
+            { nav_link: "/solve/operations", nav_text: "Operations" },
+            { nav_link: "/solve/design", nav_text: "Web Design & UX" },
+            { nav_link: "/solve/cyber-security", nav_text: "Cyber Security" },
+            { nav_link: "/solve/accessibility", nav_text: "Digital Accessibility" },
+            { nav_link: "/solve/ai", nav_text: "AI & Emerging Tech" },
+          ] },
+        ],
+      } },
+      { nav_link: "/learn", nav_text: "Learn", sub_nav: {
+        style: "tile_row", items: [
+          { nav_link: "/learn/user-groups", nav_text: "Meetups" },
+          { nav_link: "/learn/resources", nav_text: "Resource Library" },
+          { nav_link: "/learn/workshops", nav_text: "Workshops" },
+          { nav_link: "/learn/case-studies", nav_text: "Case Studies" },
+        ]
+      } },
+      { nav_link: "/join", nav_text: "Join", sub_nav: {
+        style: "icon_grid", items: [
+          {
+            nav_link: "/join/individuals",
+            nav_text: "Individual Contributors & Contractors",
+            nav_details: "Whether you freelance or work for another company, you can still find community with us.",
+            icon: "fa-users-viewfinder",
+          },
+          {
+            nav_link: "/join/small-businesses",
+            nav_text: "Small Businesses",
+            nav_details: "We help you grow your business sustainably and provide the backup you need.",
+            icon: "fa-briefcase",
+          },
+          {
+            nav_link: "/join/entrepreneurs",
+            nav_text: "Startups & Entrepreneurs",
+            nav_details: "Connect with other founders and get the resources you need to succeed.",
+            icon: "fa-forklift",
+          },
+          {
+            nav_link: "/join/partnerships",
+            nav_text: "Local Government & Organizations",
+            nav_details: "Build a stronger workforce and community with our help.",
+            icon: "fa-city",
+          },
+        ]
+      } },
     ]
   end
 end
