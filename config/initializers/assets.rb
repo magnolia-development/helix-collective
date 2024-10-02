124b5a245486e7e8037459ea3616744ea1853682
Malak Janus <992688+malakada@users.noreply.github.com>
2024-10-02 12:41:36 -0700

    Update asset pipeline for video and license poster video

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,4 +3,5 @@
 Rails.application.config.assets.version = "1.0"
 
 Rails.application.config.assets.paths << Rails.root.join("app/assets/fonts")
+Rails.application.config.assets.paths << Rails.root.join("app/assets/videos")
 
