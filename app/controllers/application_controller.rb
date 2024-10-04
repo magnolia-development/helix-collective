e428af24159b5b0dfc9c42065580ea4fddf4e715
Malak Janus <992688+malakada@users.noreply.github.com>
2024-10-04 16:00:43 -0700

    Configure Maglev to redirect to sign in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,15 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  rescue_from Maglev::Errors::NotAuthorized, with: :unauthorized_maglev
+
+  private
+
+  def unauthorized_maglev
+    flash[:error] = "You're not authorized to access the Maglev editor!"
+    # redirect_to new_user_session_path # use your own url
+    redirect_to "/auth/login"
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/*
@@ -12,6 +11,9 @@ Rails.application.routes.draw do
   #
   # get "/solve" => "home#solve_index"
   # get "/solve/:id" => "home#solve_page"
+  devise_for :users, path: "auth",
+    path_names: { sign_in: "login", sign_out: "logout", password: "secret", confirmation: "verification", unlock: "unblock", registration: "register", sign_up: "new" }
+
   mount Maglev::Engine => "/maglev"
   get "/sitemap", to: "maglev/sitemap#index", defaults: { format: "xml" }
   get "(*path)", to: "maglev/page_preview#index", defaults: { path: "index" },
